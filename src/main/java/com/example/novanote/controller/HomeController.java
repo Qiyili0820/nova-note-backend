8fb7929315a9c2649f1a2d468196e8b34d1de8c9
Qiyili0820 <qli01813@gmail.com>
2025-05-09 10:31:34 +0800

    新增 HomeController，設定根目錄返回訊息

---
+++ src/main/java/com/example/novanote/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.novanote.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Nova Note Backend is running!";
+    }
+}
